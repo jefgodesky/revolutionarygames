b85cae2c1f47d212b6c08e3a168265c9a1dd1a59
Jason Godesky <jefgodesky@gmail.com>
2026-02-27 18:32:07 -0500

    Make it a static site
    For some reason we're chewing through a huge and completely unnecessary amount of memory and then getting killed, so let's make it nice, simple, quick, small, and static to avoid that.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  output: 'static',
   site: 'https://revolutionarygames.co'
 });
